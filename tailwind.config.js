c1f71450b5b50ac02b4684d6ea4bc11a8b965102
agudamonte <agu.damonte@gmail.com>
2022-12-15 15:47:08 -0300

    recomendation section done. Scroll X installed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,44 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        bali: "url('../dist/img/bali.jpg')",
+        chicago: "url('../dist/img/chicago.jpg')",
+        europe: "url('../dist/img/europe.jpg')",
+        iceland: "url('../dist/img/iceland.jpg')",
+        LA: "url('../dist/img/LA.jpg')",
+        miami: "url('../dist/img/miami.jpg')",
+        newYork: "url('../dist/img/new_york.jpg')",
+        norway: "url('../dist/img/norway.jpg')",
+        sanFrancisco: "url('../dist/img/sanFrancisco.jpg')",
+        sanFranciscoDesktop: "url('../dist/img/sanFranciscoDesktop.jpg')",
+        seattle: "url('../dist/img/seattle.jpg')",
+        switzerland: "url('../dist/img/switzerland.jpg')",
+        sydney: "url('../dist/img/sydney.jpg')",
+        yosemite: "url('../dist/img/yosemite.jpg')",
+      },
+      backgroundColor: (theme) => ({
+        ...theme("colors"),
+        primary: "#CC2D4A",
+        secondary: "#8FA206",
+        thirdColor: "#61AEC9",
+      }),
+      textColor: {
+        primary: "#CC2D4A",
+        secondary: "#8FA206",
+        thirdColor: "#61AEC9",
+      },
+      fontFamily: {
+        Montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+    require("tailwind-scrollbar-hide"),
+    // ...
+  ],
 };
